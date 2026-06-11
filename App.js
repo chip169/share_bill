91222f536fa1836de6c0db0710f738ef2108c50c
kim xuan hieu <hieukxhe186118@fpt.edu.vn>
2026-06-11 15:41:49 +0700

    Update App

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from "react";
-import HistoryScreen from "./screens/HistoryScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  const [currentScreen, setCurrentScreen] = useState("history"); // Mặc định mở màn lịch sử trước
-
-  if (currentScreen === "history") {
-    return <HistoryScreen onNavigate={setCurrentScreen} />;
-  }
-
-import React, { useState } from "react";
 import HomeScreen from "./screens/HomeScreen";
 import HistoryScreen from "./screens/HistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import BillDetailScreen from "./screens/BillDetailScreen";
 
-
 export default function App() {
-  const [currentScreen, setCurrentScreen] = useState("home"); // Mặc định mở màn trang chủ trước
+  const [currentScreen, setCurrentScreen] = useState("home");
 
   if (currentScreen === "home") {
     return <HomeScreen onNavigate={setCurrentScreen} />;
@@ -36,18 +25,24 @@ export default function App() {
     return <BillDetailScreen onNavigate={setCurrentScreen} />;
   }
 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Màn hình không tồn tại</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    justify: "center",
+    backgroundColor: "#ecf0f1",
     padding: 8,
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
