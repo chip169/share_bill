930efd3f823e2f321582b8e633d0de7bf057237f
kim xuan hieu <hieukxhe186118@fpt.edu.vn>
2026-06-18 00:47:59 +0700

    Update Header.js

--- components/profile/Header.js
+++ components/profile/Header.js
@@ -8,7 +8,7 @@ const Header = ({ user }) => {
 
   return (
     <View style={tw`bg-[#0984e3] pt-14 pb-20 px-6 rounded-b-[32px] items-center`}>
-      <Text style={tw`text-white text-2xl font-bold align-self-start mb-5`}>Tài khoản</Text>
+      <Text style={tw`text-white text-2xl font-bold self-start mb-5`}>Tài khoản</Text>
       <View style={tw`w-24 h-24 bg-white rounded-full items-center justify-center shadow-lg border-2 border-white/50 mb-3`}>
         <Text style={tw`text-[#00b894] text-3xl font-bold`}>{firstLetter}</Text>
       </View>
